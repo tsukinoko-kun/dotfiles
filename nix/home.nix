396e1df89b412006af177e834e01646a6b77684c
Frank Mayer <gh@frankmayer.dev>
2024-10-13 11:30:18 +0200

    vi mode

--- nix/home.nix
+++ nix/home.nix
@@ -129,5 +129,12 @@
   programs.zsh = {
     enable = true;
     syntaxHighlighting.enable = true;
+    plugins = [
+      {
+        name = "vi-mode";
+        src = pkgs.zsh-vi-mode;
+        file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+      }
+    ];
   };
 }
