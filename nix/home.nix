bb82dbeaa6c2527be554ba0cae2ebe4f270c0854
Frank Mayer <gh@frankmayer.dev>
2024-11-16 15:32:33 +0100

    wezterm auto theme

--- nix/home.nix
+++ nix/home.nix
@@ -44,8 +44,22 @@
   programs.wezterm = {
     enable = true;
     extraConfig = ''
-      local wezterm = require("wezterm")
       local mux = wezterm.mux
+
+      local function get_appearance_mode()
+        -- Run osascript to get the appearance
+        local handle =
+          io.popen("osascript -e 'tell application \"System Events\" to tell appearance preferences to return dark mode'")
+        if not handle then
+          return "error"
+        end
+        local result = handle:read("*a")
+        handle:close()
+
+        -- Trim result and convert to boolean
+        result = result:match("^%s*(.-)%s*$")
+        return result == "true" and "dark" or "light"
+      end
 
       wezterm.on("gui-startup", function(cmd)
         local _, _, window = mux.spawn_window(cmd or {})
@@ -58,7 +72,7 @@
         config = wezterm.config_builder()
       end
 
-      config.color_scheme = "Catppuccin Mocha"
+      config.color_scheme = get_appearance_mode() == "dark" and "Catppuccin Mocha" or "Catppuccin Latte"
       config.default_cursor_style = "SteadyBar"
 
       config.window_padding = {
