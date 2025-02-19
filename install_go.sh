197f2f498979cf9e17dbf245611793750a3c20e1
Frank Mayer <gh@frankmayer.dev>
2025-02-19 05:01:30 +0100

    golang cobra

--- install_go.sh
+++ install_go.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 /opt/homebrew/bin/go install github.com/air-verse/air@latest
 /opt/homebrew/bin/go install github.com/a-h/templ/cmd/templ@latest
+/opt/homebrew/bin/go install github.com/spf13/cobra-cli@latest
