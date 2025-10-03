e2a7c0e2dca1ec4e96016a5e8f90ff573c3566ed
Frank Mayer <gh@frankmayer.dev>
2025-10-03 02:23:42 +0200

    update

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -77,4 +77,12 @@ return {
         end,
     },
     { "RaafatTurki/hex.nvim" },
+    {
+        "3rd/image.nvim",
+        build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+        opts = {
+            backend = "kitty",
+            processor = "magick_cli",
+        },
+    },
 }
